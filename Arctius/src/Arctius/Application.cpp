06128642bc98cd4535c151155e7d2768ea2fede0
Jamon Jordan <jamonj@protonmail.com>
2022-07-03 17:16:31 +0200

    Added the event system

--- Arctius/src/Arctius/Application.cpp
+++ Arctius/src/Arctius/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Events/ApplicationEvent.h"
+#include "Log.h"
+
 namespace Arctius {
 
 	Application::Application()
@@ -14,6 +17,16 @@ namespace Arctius {
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(2180, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			ARC_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			ARC_TRACE(e);
+		}
+
 		while (true);
 	}
 
--- Arctius/src/Arctius/Log.h
+++ Arctius/src/Arctius/Log.h
@@ -6,6 +6,7 @@
 // Including the spdlog headers
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/fmt/ostr.h"
 
 namespace Arctius {
 
